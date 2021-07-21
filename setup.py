b13d5deb9e84adc5b84e41afebea7b4cd196e41a
Jhony Kaesemodel Pontes <jpontes@argo.ai>
2021-07-21 10:12:52 -0400

    Changes cibuild

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif platform.system() == "Linux":
 else:  # macos
     library_dirs += [osp.join(conda_fpath, "lib")]
     libraries = ["opencv_core", "opencv_imgproc"]
-    opencv_fpath = osp.join(conda_fpath, "include", "opencv4")
+    opencv_fpath = osp.join(conda_env_fpath, "include", "opencv4")
 include_dirs += [opencv_fpath]
 
 
